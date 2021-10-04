868b372313d836f6ecd2bdb3e8dc96a05eb457d1
Gabriel Ariza Gomes de Castro <ligados.adwords@gmail.com>
2021-10-04 19:36:14 +0000

    try to get sass

--- app/Views/App/Partials/menu.php
+++ app/Views/App/Partials/menu.php
@@ -5,8 +5,7 @@ $identifier = $identifier ?? null;
   <a class="navbar-brand" href="<?= site_url("/") ?>">
     <img class="my-3 ml-3" src="<?= sys_config()->variables['logo'] ?>" alt="<?= sys_config()->title ?>">
   </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="navbarNav"
-          aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="main-menu">
@@ -15,8 +14,7 @@ $identifier = $identifier ?? null;
         <a class="nav-link" href="<?= site_url('quem-somos') ?>">Quem Somos</a>
       </li>
       <li class="nav-item dropdown  <?= $identifier == 'imoveis' ? 'active' : '' ?>">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-           aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Imóveis
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -34,15 +32,19 @@ $identifier = $identifier ?? null;
       <li class="nav-item <?= $identifier == 'faq' ? 'active' : '' ?>">
         <a class="nav-link" href="<?= site_url('faq') ?>">FAQ</a>
       </li>
-<!--      <li class="nav-item --><?//= $identifier == 'blog' ? 'active' : '' ?><!--">-->
-<!--        <a class="nav-link" href="--><?//= site_url('blog') ?><!--">Blog</a>-->
-<!--      </li>-->
+      <!--      <li class="nav-item --><? //= $identifier == 'blog' ? 'active' : '' 
+                                        ?>
+      <!--">-->
+      <!--        <a class="nav-link" href="--><? //= site_url('blog') 
+                                                ?>
+      <!--">Blog</a>-->
+      <!--      </li>-->
       <li class="nav-item <?= $identifier == 'contato' ? 'active' : '' ?>">
         <a class="nav-link" href="<?= site_url('contato') ?>">Contato</a>
       </li>
-<!--      <li class="nav-item d-flex align-items-center">-->
-<!--        <a class="btn btn-secondary rounded-pill" href="#">Simulador de Financiamento</a>-->
-<!--      </li>-->
+      <li class="nav-item d-flex align-items-center">
+        <a class="btn btn-secondary rounded-pill" href="<?= site_url('simulador') ?>">Simulador de Financiamento</a>
+      </li>
     </ul>
   </div>
 </nav>
